1cfc11e934eb0bfce22ae5ae4f9ab5a21a4e7df2
Georgi Martsenkov <georgi.martsenkov@vodafone.com>
2019-06-07 23:16:58 +0100

    init config

---
+++ init.el
@@ -0,0 +1,94 @@
+(package-initialize)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (doom-tomorrow-night)))
+ '(custom-safe-themes
+   (quote
+    ("10461a3c8ca61c52dfbbdedd974319b7f7fd720b091996481c8fb1dded6c6116" "75d3dde259ce79660bac8e9e237b55674b910b470f313cdf4b019230d01a982a" "a3fa4abaf08cc169b61dea8f6df1bbe4123ec1d2afeb01c17e11fdc31fc66379" "fd944f09d4d0c4d4a3c82bd7b3360f17e3ada8adf29f28199d09308ba01cc092" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "49ec957b508c7d64708b40b0273697a84d3fee4f15dd9fc4a9588016adee3dad" "9954ed41d89d2dcf601c8e7499b6bb2778180bfcaeb7cdfc648078b8e05348c6" "6d589ac0e52375d311afaa745205abb6ccb3b21f6ba037104d71111e7e76a3fc" "fe666e5ac37c2dfcf80074e88b9252c71a22b6f5d2f566df9a7aa4f9bea55ef8" "a8c210aa94c4eae642a34aaf1c5c0552855dfca2153fa6dd23f3031ce19453d4" "f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "8aca557e9a17174d8f847fb02870cb2bb67f3b6e808e46c0e54a44e3e18e1020" "6b289bab28a7e511f9c54496be647dc60f5bd8f9917c9495978762b99d8c96a0" "43c808b039893c885bdeec885b4f7572141bd9392da7f0bd8d8346e02b2ec8da" "93a0885d5f46d2aeac12bf6be1754faa7d5e28b27926b8aa812840fe7d0b7983" "151bde695af0b0e69c3846500f58d9a0ca8cb2d447da68d7fbf4154dcf818ebc" "d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370" "cd736a63aa586be066d5a1f0e51179239fe70e16a9f18991f6f5d99732cabb32" default)))
+ '(doom-modeline-mode t)
+ '(package-selected-packages
+   (quote
+    (exec-path-from-shell alchemist company elixir-mode magit ace-window dashboard doom-themes doom-modeline which-key ivy rspec-mode ruby-end projectile use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(load-file "~/.emacs.d/common.el")
+(load-theme 'doom-tomorrow-night t)
+
+;; MacOS Path Fix
+(use-package exec-path-from-shell :ensure t)
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize)
+  (exec-path-from-shell-copy-env "GOPATH"))
+
+
+;; UI
+(use-package doom-modeline
+      :ensure t
+      :hook (after-init . doom-modeline-mode))
+
+;; DASHBOARD
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook))
+(setq dashboard-center-content t)
+(setq dashboard-startup-banner '2)
+(setq dashboard-items '((recents  . 5)
+                        (projects . 5)
+                        (bookmarks . 5)
+                        (agenda . 5)))
+                        
+;; PROJECTILE
+(use-package projectile :ensure t)
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(setq projectile-completion-system 'ivy)
+
+;; Company
+(use-package company :ensure t)
+(setq company-tooltip-limit 10) 
+(setq company-idle-delay .1)
+(setq company-tooltip-align-annotations t)
+(global-company-mode t)
+
+;; IVY
+(use-package ivy :ensure t)
+(ivy-mode 1)
+
+;; RUBY
+(use-package ruby-end :ensure t)
+(use-package rspec-mode :ensure t)
+(setq ruby-indent-level 2)
+(setq rspec-primary-source-dirs '("app"))
+
+;; WHICH-KEY
+(use-package which-key :ensure t)
+(which-key-mode)
+
+;; ACE WINDOWS
+(use-package ace-window :ensure t)
+(global-set-key (kbd "M-o") 'ace-window)
+
+;; MAGIT
+(use-package magit :ensure t)
+(setq magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1)
+
+;; Elixir
+(use-package elixir-mode :ensure t)
+(use-package alchemist :ensure t)
