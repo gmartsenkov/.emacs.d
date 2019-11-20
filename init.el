195754525e28b1e75eae061faf045b7ac84d9118
Georgi Martsenkov <georgi.martsenkov@vodafone.com>
2019-11-20 11:16:36 +0000

    adds robe

--- init.el
+++ init.el
@@ -91,12 +91,15 @@
 (use-package rspec-mode :ensure t)
 (use-package rubocop :ensure t)
 (use-package inf-ruby :ensure t)
+(use-package robe :ensure t)
 (add-hook 'ruby-mode-hook #'rubocop-mode)
 (add-hook 'after-init-hook 'inf-ruby-switch-setup)
 (setq ruby-indent-level 2)
 (setq rspec-primary-source-dirs '("app"))
 (setq ruby-insert-encoding-magic-comment nil)
-
+(add-hook 'ruby-mode-hook 'robe-mode)
+(eval-after-load 'company
+  '(push 'company-robe company-backends))
 ;; WHICH-KEY
 ;;(use-package which-key :ensure t)
 ;;(which-key-mode)
@@ -185,7 +188,7 @@
     ("bd6ced8badda12f95e16e641d76d861de096c691720ede6388a226914e97cf23" "2d392972cbe692ee4ac61dc79907af65051450caf690a8c4d36eb40c1857ba7d" "7d56fb712ad356e2dacb43af7ec255c761a590e1182fe0537e1ec824b7897357" "c8f959fb1ea32ddfc0f50db85fea2e7d86b72bb4d106803018be1c3566fd6c72" "1728dfd9560bff76a7dc6c3f61e9f4d3e6ef9d017a83a841c117bd9bebe18613" "728eda145ad16686d4bbb8e50d540563573592013b10c3e2defc493f390f7d83" default)))
  '(package-selected-packages
    (quote
-    (inf-ruby rubocop magit yasnippet-snippets ws-butler which-key use-package tide swiper smartparens shell-pop ruby-end rspec-mode robe projectile ivy-rich indium forge flycheck-pos-tip flycheck-clojure expand-region exec-path-from-shell elixir-yasnippets doom-themes doom-modeline diff-hl dashboard crystal-mode anzu ample-theme alchemist ag ace-window ace-jump-mode ac-js2))))
+    (smex inf-ruby rubocop magit yasnippet-snippets ws-butler which-key use-package tide swiper smartparens shell-pop ruby-end rspec-mode robe projectile ivy-rich indium forge flycheck-pos-tip flycheck-clojure expand-region exec-path-from-shell elixir-yasnippets doom-themes doom-modeline diff-hl dashboard crystal-mode anzu ample-theme alchemist ag ace-window ace-jump-mode ac-js2))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
