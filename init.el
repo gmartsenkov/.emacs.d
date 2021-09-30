f55de9da1c9b37a1692d309ea93374f30c973b05
Georgi Martsenkov <g.martsenkov@gmail.com>
2021-09-30 23:38:48 +0100

    spacemacs theme

--- init.el
+++ init.el
@@ -97,15 +97,20 @@
   :init
   (add-hook 'rspec-mode-hook 'inf-ruby-switch-setup))
 (use-package rubocop :ensure t)
-  
+(use-package spacemacs-theme
+  :defer t
+  :init
+  (load-theme 'spacemacs-dark t))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
  '(package-selected-packages
-   '(rubocop rspec-mode bundler parseedn which-key cider ivy evil-collection evil use-package)))
+   '(spacemacs-theme rubocop rspec-mode bundler parseedn which-key cider ivy evil-collection evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
