36c984e6c70bd0c1bdd356b7c0fc65afa5401394
Georgi Martsenkov <g.martsenkov@gmail.com>
2021-10-03 10:05:32 +0100

    doom-modeline

--- init.el
+++ init.el
@@ -116,6 +116,14 @@
   :init
   (load-theme 'spacemacs-dark t))
 
+(use-package doom-modeline
+  :ensure t
+  :config
+  (setq doom-modeline-major-mode-icon nil)
+  (setq doom-modeline-buffer-encoding nil)
+  :init
+  (doom-modeline-mode))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -124,7 +132,7 @@
  '(custom-safe-themes
    '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
  '(package-selected-packages
-   '(diminish simple-modeline spacemacs-theme rubocop rspec-mode bundler parseedn which-key cider ivy evil-collection evil use-package)))
+   '(doom-modeline diminish simple-modeline spacemacs-theme rubocop rspec-mode bundler parseedn which-key cider ivy evil-collection evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
