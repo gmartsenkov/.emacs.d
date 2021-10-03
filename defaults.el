22d4b238822eb6ccd1af281712ff3c817eaa9f75
Georgi Martsenkov <g.martsenkov@gmail.com>
2021-10-03 09:31:24 +0100

    performance tweaks as lsp guide

--- defaults.el
+++ defaults.el
@@ -2,6 +2,7 @@
 (set-language-environment "UTF-8")
 (set-default-coding-systems 'utf-8)
 (tool-bar-mode -1)
+(scroll-bar-mode -1)
 (display-time)
 (setq delete-old-versions -1 )
 (setq backup-directory-alist `(("." . "~/.emacs.d/backups")) )
@@ -16,3 +17,8 @@
 (setq ruby-indent-level 2)
 (setq ruby-insert-encoding-magic-comment nil)
 (setq rspec-primary-source-dirs '("app"))
+
+;; Performance
+(setq gc-cons-threshold 100000000)
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
+(setq native-comp-deferred-compilation t)
