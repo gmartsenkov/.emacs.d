cd15cca58dd4725bb04571ba64ed535037d0ea49
Georgi Martsenkov <g.martsenkov@gmail.com>
2021-10-11 17:53:55 +0100

    add padding

--- defaults.el
+++ defaults.el
@@ -23,3 +23,24 @@
 (setq gc-cons-threshold 100000000)
 (setq read-process-output-max (* 1024 1024)) ;; 1mb
 (setq native-comp-deferred-compilation t)
+
+(require 'disp-table)
+(setq default-frame-alist
+      (append (list
+	           '(min-height . 1)
+               '(height     . 45)
+	           '(min-width  . 1)
+               '(width      . 81)
+               '(vertical-scroll-bars . nil)
+               '(internal-border-width . 24)
+               '(left-fringe    . 1)
+               '(right-fringe   . 1)
+               '(tool-bar-lines . 0)
+               '(menu-bar-lines . 0))))
+
+(setq x-underline-at-descent-line t)
+
+;; Vertical window divider
+(setq window-divider-default-right-width 24)
+(setq window-divider-default-places 'right-only)
+(window-divider-mode 1)
