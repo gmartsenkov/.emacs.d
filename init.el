28fe41cffd02ed07d0bdcacb8b35aac43369c810
Georgi Martsenkov <g.martsenkov@gmail.com>
2021-10-11 17:57:10 +0100

    use diff-hl instead of git-gitter-fringe

--- init.el
+++ init.el
@@ -196,30 +196,9 @@
   (persp-mode))
 
 (use-package git-link :ensure t)
-(use-package git-gutter :ensure t)
-(use-package git-gutter-fringe
-  :diminish git-gutter-mode
-  :after git-gutter
-  :demand fringe-helper
+(use-package diff-hl
   :ensure t
-  :init
-  (add-hook 'text-mode-hook #'git-gutter-mode)
-  (add-hook 'prog-mode-hook #'git-gutter-mode)
-  (add-hook 'conf-mode-hook #'git-gutter-mode)
-  :config
-  ;; subtle diff indicators in the fringe
-  ;; places the git gutter outside the margins.
-  (setq-default fringes-outside-margins t)
-  ;; thin fringe bitmaps
-  (define-fringe-bitmap 'git-gutter-fr:added
-    [224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224]
-    nil nil 'center)
-  (define-fringe-bitmap 'git-gutter-fr:modified
-    [224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224]
-    nil nil 'center)
-  (define-fringe-bitmap 'git-gutter-fr:deleted
-    [0 0 0 0 0 0 0 0 0 0 0 0 0 128 192 224 240 248]
-    nil nil 'center))
+  :init (global-diff-hl-mode))
 
 (use-package yasnippet
   :ensure t
@@ -238,18 +217,3 @@
 
 (use-package elixir-mode :ensure t)
 (use-package alchemist :ensure t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "5784d048e5a985627520beb8a101561b502a191b52fa401139f4dd20acb07607" "b0e446b48d03c5053af28908168262c3e5335dcad3317215d9fdeb8bac5bacf9" "e8df30cd7fb42e56a4efc585540a2e63b0c6eeb9f4dc053373e05d774332fc13" "7eea50883f10e5c6ad6f81e153c640b3a288cd8dc1d26e4696f7d40f754cc703" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
- '(package-selected-packages
-   '(flycheck-clj-kondo nano-modeline elixir-mode ag anzu yasnippet-snippets yasnippet doom-themes cider :clojure-mode lsp-ivy git-gutter-fringe git-link perspective doom-modeline diminish simple-modeline spacemacs-theme rubocop rspec-mode bundler parseedn which-key ivy evil-collection evil use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
