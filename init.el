aea81cce5f975ed3a6542ee913cea241c79e533f
Georgi Martsenkov <g.martsenkov@gmail.com>
2021-10-11 18:00:09 +0100

    add magit hooks to diff-hl-mode

--- init.el
+++ init.el
@@ -198,7 +198,10 @@
 (use-package git-link :ensure t)
 (use-package diff-hl
   :ensure t
-  :init (global-diff-hl-mode))
+  :init
+  (global-diff-hl-mode)
+  (add-hook 'magit-pre-refresh-hook 'diff-hl-magit-pre-refresh)
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
 
 (use-package yasnippet
   :ensure t
