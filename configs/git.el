340dada33cd2fbdc0cd135d15eb2786585fbb9d6
Georgi Martsenkov <g.martsenkov@gmail.com>
2022-11-30 22:02:23 +0000

    Auto insert mode in magit commit buffer

--- configs/git.el
+++ configs/git.el
@@ -1,7 +1,8 @@
 (use-package magit
   :ensure t
   :config
-  (setq magit-display-buffer-function 'magit-display-buffer-traditional))
+  (setq magit-display-buffer-function 'magit-display-buffer-traditional)
+  (add-hook 'git-commit-mode-hook 'evil-insert-state))
 
 (use-package forge :ensure t :after magit)
 (use-package git-link :ensure t)
