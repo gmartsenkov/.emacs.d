b5ee05857900a6b98789743b9e10a86fdd1a6816
Georgi Martsenkov <g.martsenkov@gmail.com>
2023-04-19 23:29:58 +0100

    add frozen snippet

--- configs/ui.el
+++ configs/ui.el
@@ -4,7 +4,7 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-tokio-night t)
+  (load-theme 'doom-tokyo-night t)
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
